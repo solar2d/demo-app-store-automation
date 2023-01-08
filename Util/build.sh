d45367e69472c754349bc6a41a736e2cd9d0883d
petiflas <petiflas@gmail.com>
2023-01-08 22:16:45 +0000

    Update build.sh

--- Util/build.sh
+++ Util/build.sh
@@ -53,7 +53,7 @@ hdiutil detach Util/S2D
 echo "Uploading the app"
 if [ "${APPLE_TEAM_ID}" ]
 then
-    xcrun iTMSTransporter -m upload -assetFile Util/*.ipa  -u "${APPLE_USERNAME}" -p "${APPLE_PASSWORD}" -asc_provider "${APPLE_TEAM_ID}"
+    xcrun altool --upload-app -f Util/*.ipa -u "${APPLE_USERNAME}" -p "${APPLE_PASSWORD}" --asc-provider "${APPLE_TEAM_ID}"
 else
     xcrun iTMSTransporter -m upload -assetFile Util/*.ipa  -u "${APPLE_USERNAME}" -p "${APPLE_PASSWORD}"
 fi
